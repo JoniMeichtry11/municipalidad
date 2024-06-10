ea0033ec72b1d6c8809ba3e9fecf83c167f8c98d
JoniMeichtry11 <joni.meichtry@gmail.com>
2024-06-10 20:35:06 -0300

    feat: add libraries and configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
+
 module.exports = {
+  important: true,
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [addDynamicIconSelectors()]
 }
-
